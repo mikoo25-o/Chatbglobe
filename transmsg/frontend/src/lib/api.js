899e69db18ddbdc82200d9db5f34a5387d0360a1
mikoo25-o <mikeotieno2026@gmail.com>
2026-05-13 11:45:00 -0500

    Update production API URLs

--- transmsg/frontend/src/lib/api.js
+++ transmsg/frontend/src/lib/api.js
@@ -3,32 +3,29 @@ import axios from 'axios'
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    'http://localhost:4000/api',
-
+    'https://chatbglobe-production.up.railway.app/api',
   timeout: 15000
 })
 
 api.interceptors.request.use((config) => {
-  const token =
-    localStorage.getItem('transmsg_token')
+  const token = localStorage.getItem('transmsg_token')
 
   if (token) {
-    config.headers.Authorization =
-      `Bearer ${token}`
+    config.headers.Authorization = `Bearer ${token}`
   }
 
   return config
 })
 
 api.interceptors.response.use(
-  (res) => res,
-  (err) => {
+  (response) => response,
+  (error) => {
     console.error(
       'API Error:',
-      err.response?.data || err.message
+      error.response?.data || error.message
     )
 
-    return Promise.reject(err)
+    return Promise.reject(error)
   }
 )
 
